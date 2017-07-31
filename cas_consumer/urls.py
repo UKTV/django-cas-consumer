9ad1592cae66e71722f40cacbb4392f29bc008ca
Ryan Stalbow <ryan.stalbow@uktv.co.uk>
2017-07-31 10:57:43 +0100

    django 1.11 fix

--- cas_consumer/urls.py
+++ cas_consumer/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import *
 
 from cas_consumer.views import *
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^login/', login, name="cas_login"),
     url(r'^logout/', logout, name="cas_logout"),
-)
+]
